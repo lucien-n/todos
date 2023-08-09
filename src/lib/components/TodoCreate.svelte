dc9e93a574e2e2147019c848576652a5413ef4cf
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-09 20:27:04 +0200

    ref: moved todo form to it's own component

---
+++ src/lib/components/TodoCreate.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+	let title: string = '';
+	let content: string = '';
+
+	const createTodo = (): void => {
+		dispatch('create', { title, content });
+	};
+</script>
+
+<section class="flex flex-col">
+	<label for="todo-title">Title</label>
+	<input
+		type="text"
+		name="title"
+		id="title"
+		class="rounded border border-black px-2 py-1"
+		required
+		maxlength="80"
+		bind:value={title}
+	/>
+</section>
+<section class="flex flex-col">
+	<label for="todo-content">Content</label>
+	<textarea
+		name="content"
+		id="content"
+		cols="30"
+		rows="2"
+		class="rounded border border-black px-2 py-1"
+		required
+		maxlength="300"
+		bind:value={content}
+	/>
+</section>
+<button
+	class="rounded border border-black px-3 py-2 outline-black hover:bg-zinc-100 active:bg-zinc-200"
+	on:click={createTodo}>Create</button
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import TodoCreate from '$lib/components/TodoCreate.svelte';
 	import TodoView from '$lib/components/TodoView.svelte';
 	import { todos } from '$lib/stores';
 	import type { Todo } from '$lib/types';
@@ -14,7 +15,11 @@
 		});
 	});
 
-	const createTodo = () => {
+	const createTodo = ({
+		detail: { title, content }
+	}: {
+		detail: { title: string; content: string };
+	}) => {
 		if (!title) return;
 
 		const todo: Todo = {
@@ -33,39 +38,11 @@
 
 <section class="flex w-full flex-col gap-y-5">
 	<section class="mx-auto flex flex-col gap-3 rounded border border-black px-3 py-2 lg:w-1/2">
-		<section class="flex flex-col">
-			<label for="todo-title">Title</label>
-			<input
-				type="text"
-				name="title"
-				id="title"
-				class="rounded border border-black px-2 py-1"
-				required
-				maxlength="80"
-				bind:value={title}
-			/>
-		</section>
-		<section class="flex flex-col">
-			<label for="todo-content">Content</label>
-			<textarea
-				name="content"
-				id="content"
-				cols="30"
-				rows="2"
-				class="rounded border border-black px-2 py-1"
-				required
-				maxlength="300"
-				bind:value={content}
-			/>
-		</section>
-		<button
-			class="rounded border border-black px-3 py-2 outline-black hover:bg-zinc-100 active:bg-zinc-200"
-			on:click={createTodo}>Create</button
-		>
+		<TodoCreate on:create={createTodo} />
 	</section>
 	<hr class="mx-auto lg:w-1/2" />
 	<section class="mx-auto flex flex-col gap-3 lg:w-1/2">
-		{#each $todos.reverse() as todo}
+		{#each $todos as todo}
 			<TodoView {todo} on:delete={deleteTodo} />
 		{/each}
 	</section>
