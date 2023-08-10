773bad63a02062e6080737ac0da3f5e9b29eb347
Lucien Neuhoff <lucien.neuhoff@protonmail.com>
2023-08-10 11:08:54 +0200

    feat: todo editing

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,20 @@ const App = () => {
     setTodos([...todos, todo]);
   };
 
+  const editTodo = (updated_todo: Todo): void => {
+    setTodos([
+      ...todos.filter((todo: Todo) => todo.id !== updated_todo.id),
+      updated_todo,
+    ]);
+  };
+
   return (
     <section className="flex w-full flex-col gap-y-5">
       <TodoCreate onCreate={createTodo}></TodoCreate>
       <hr className="mx-auto lg:w-1/2" />
       <section className="mx-auto flex flex-col gap-3 lg:w-1/2">
         {todos.map((todo: Todo) => (
-          <TodoView key={todo.id} todo={todo}></TodoView>
+          <TodoView key={todo.id} todo={todo} onEdit={editTodo}></TodoView>
         ))}
       </section>
     </section>
--- src/components/TodoView.tsx
+++ src/components/TodoView.tsx
@@ -1,18 +1,87 @@
+import { useState } from "react";
 import { Todo } from "../lib/types";
 
-const TodoView = ({ todo }: { todo: Todo }) => {
+const TodoView = ({
+  todo,
+  onEdit,
+}: {
+  todo: Todo;
+  onEdit: (todo: Todo) => void;
+}) => {
+  const [editing, setEditing] = useState<boolean>(false);
+
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+
+  const toggleEditing = (): void => {
+    setEditing(!editing);
+  };
+
+  const editTodo = () => {
+    if (!title) return;
+
+    todo.title = title;
+    todo.content = content;
+
+    onEdit(todo);
+    toggleEditing();
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value;
+    if (!value) return;
+    setTitle(value);
+  };
+
+  const handleTextAreaChange = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    const value = e.target.value;
+    if (!value) return;
+    setContent(value);
+  };
+
   return (
     <article
       id="todo-{todo.id}"
       className="flex w-full gap-3 rounded border border-black px-3 py-2"
     >
       <div className="flex w-full flex-col gap-2">
-        <h1 className="border-b border-white text-xl font-bold">
-          {todo.title}
-        </h1>
-        <p className="border-b border-white">{todo.content}</p>
+        {editing ? (
+          <>
+            <input
+              type="text"
+              className="border-b border-black text-xl font-bold"
+              defaultValue={todo.title}
+              onChange={handleInputChange}
+            />
+            <textarea
+              rows={2}
+              className="border-b border-black"
+              defaultValue={todo.content}
+              onChange={handleTextAreaChange}
+            />
+          </>
+        ) : (
+          <>
+            <h1 className="border-b border-white text-xl font-bold">
+              {todo.title}
+            </h1>
+            <p className="border-b border-white">{todo.content}</p>
+          </>
+        )}
       </div>
-      <div className="flex flex-col justify-center"></div>
+      <div className="flex flex-col justify-center">
+        {editing ? (
+          <button className=" text-blue-500 underline" onClick={editTodo}>
+            Save
+          </button>
+        ) : (
+          <button className=" text-blue-500 underline" onClick={toggleEditing}>
+            Edit
+          </button>
+        )}
+      </div>
     </article>
   );
 };
